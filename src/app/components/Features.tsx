42a69369979039af477715de51fec99ba61f7921
araczkowski <araczkowski@gmail.com>
2026-01-30 17:03:44 +0100

    hacking

--- src/app/components/Features.tsx
+++ src/app/components/Features.tsx
@@ -9,7 +9,8 @@ import {
   Film,
   Code2,
   Smartphone,
-  Layers,
+  ShieldCheck,
+  FileText,
 } from "lucide-react";
 
 const features = [
@@ -45,26 +46,17 @@ const features = [
     description: "Twój osobisty, energooszczędny serwer deweloperski.",
   },
   {
+    icon: <ShieldCheck className="w-8 h-8" />,
+    title: "Hakowanie i testy",
+    description:
+      "Idealne narzędzie do nauki cyberbezpieczeństwa i testów penetracyjnych.",
+  },
+  {
     icon: <Smartphone className="w-8 h-8" />,
     title: "Android",
     description: "Dostępne obrazy Android TV oraz Android AOSP.",
   },
-  {
-    icon: <Layers className="w-8 h-8" />,
-    title: "Wszechstronność",
-    description: (
-      <>
-        Jeden sprzęt, nieskończone możliwości zastosowań.{" "}
-        <a
-          href="/docs"
-          rel="noopener noreferrer"
-          className="text-blue-600 hover:underline"
-        >
-          Dokumentacja
-        </a>
-      </>
-    ),
-  },
+
 ];
 
 export default function Features() {
@@ -79,16 +71,28 @@ export default function Features() {
           whileInView={{ opacity: 1, y: 0 }}
           viewport={{ once: true }}
           transition={{ duration: 0.6 }}
-          className="text-center mb-24"
+          className="mb-24 flex flex-col md:flex-row justify-between items-center text-center md:text-left"
         >
-          <h2 className="text-4xl md:text-6xl font-semibold tracking-tight mb-6">
-            Możliwości. <span className="text-gray-400">Nieskończone.</span>
-          </h2>
-          <p className="text-xl text-gray-500 max-w-3xl mx-auto leading-relaxed">
-            Od serwera domowego po centrum multimedialne. Linux Box dostosowuje
-            się do Twoich potrzeb, oferując wydajność i elastyczność, której
-            oczekujesz.
-          </p>
+          <div className="max-w-3xl md:w-2/3">
+            <h2 className="text-4xl md:text-6xl font-semibold tracking-tight mb-6">
+              Możliwości. <span className="text-gray-400">Nieskończone.</span>
+            </h2>
+            <p className="text-xl text-gray-500 mx-auto md:mx-0 leading-relaxed">
+              Od serwera domowego po centrum multimedialne. Linux Box dostosowuje
+              się do Twoich potrzeb, oferując wydajność i elastyczność, której
+              oczekujesz.
+            </p>
+          </div>
+          <div className="mt-8 md:mt-0 md:w-1/3 flex justify-center md:justify-end">
+            <a
+              href="/docs"
+              rel="noopener noreferrer"
+              className="flex items-center text-blue-600 hover:underline text-lg"
+            >
+              <FileText className="mr-2 h-5 w-5" />
+              <span className="font-bold">Przejdź do dokumentacji</span>
+            </a>
+          </div>
         </motion.div>
 
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-x-8 gap-y-16">
